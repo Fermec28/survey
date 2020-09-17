d32177ea215364c46a9794cf28e18a43dee058ba
Ferney Medina <fermed28@gmail.com>
2020-09-17 00:38:52 -0500

    fix allow all origins if environment variable is not defined

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['host_origin']
+    origins ENV['host_origin'] || "*"
 
     resource '*',
       headers: :any,
